5d3359c1026f0e2a309474a2470136a8e72f2b5b
Harry Bromley <harrybromley@btinternet.com>
2025-12-03 13:37:57 +0000

    Day 1 solution

---
+++ day-1/src/main.rs
@@ -0,0 +1,78 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn parse_file(name: &str) -> Vec<i32> {
+    let file = File::open(name).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut rotations = Vec::new();
+
+    for line in reader.lines() {
+        let mut line = line.unwrap();
+        let direction = line.remove(0);
+        let mut value: i32 = line.parse().unwrap();
+        if direction == 'L' {
+            value = -value;
+        }
+        rotations.push(value);
+    }
+    rotations
+}
+
+fn count_zero_stops(start_value: i32, rotations: &Vec<i32>) -> u32 {
+    let mut current_value = start_value;
+    let mut n_zeros = 0;
+
+    for value in rotations {
+        current_value += value;
+        current_value = current_value.rem_euclid(100);
+        if current_value == 0 {
+            n_zeros += 1
+        }
+    }
+
+    n_zeros
+}
+
+fn count_zero_clicks(start_value: i32, rotations: &Vec<i32>) -> u32 {
+    let mut current_value = start_value;
+    let mut n_clicks = 0;
+
+    for value in rotations {
+        // Update value
+        let mut new_value = current_value + value;
+        // Count a click if the updated value is zero
+        if new_value == 0 {
+            n_clicks += 1;
+        }
+
+        // If the dial has looped around then count additional clicks from this
+        if new_value < 0 || new_value > 99 {
+            // Remove a click when starting from zero and becoming negative as
+            // this should not be counted
+            if current_value == 0 && *value < 0 {
+                n_clicks -= 1;
+            }
+            n_clicks += new_value.div_euclid(100).abs() as u32;
+            new_value = new_value.rem_euclid(100);
+
+            // When finishing on zero in the negative case an additional click
+            // should also be added
+            if new_value == 0 && *value < 0 {
+                n_clicks += 1;
+            }
+        }
+        current_value = new_value
+    }
+
+    n_clicks
+}
+
+fn main() {
+    let rotations = parse_file("input.txt");
+    let n_zero_stops = count_zero_stops(50, &rotations);
+    let n_zero_clicks = count_zero_clicks(50, &rotations);
+
+    println!("Number of times stopped at zero = {}", n_zero_stops);
+    println!("Number of zero clicks = {}", n_zero_clicks);
+}
