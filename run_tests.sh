a2d487b28fa6824afa6d8d2802d5fc3419408a69
Harry Bromley <harrybromley@btinternet.com>
2025-12-08 18:03:52 +0000

    Bash script to run crate tests for all days

---
+++ run_tests.sh
@@ -0,0 +1,17 @@
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+for i in {1..12}; 
+do 
+    crate_name="day-$i"
+    if [ -d "$crate_name" ]; then 
+        echo -e "${BLUE}Day $i RUNNING${NC}"
+        if (cd "$crate_name" && cargo test --release --quiet); then
+            echo -e "${GREEN}Day $i PASSED ${NC}\n\n"
+        else
+            echo -e "${RED}Day $i FAILED ${NC}\n\n"
+        fi  
+    fi
+done
